56792830d7a4e3a8f41c6165809cba760ca10b3c
Tomerkm <tomerkeiynan@gmail.com>
2016-04-16 02:19:11 +0300

    updaters

---
+++ Gui.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import Nmea_To_Csv
+import Nmea_To_kml
+
+root = Tk()
+root.title('GUI')
+t = StringVar()
+Label(root,width=30,text='Insert Your Source File Here:').pack(side=LEFT, padx=0)
+Entry(root,width=85,textvariable=t, bg='white').pack(side=LEFT, padx=50)
+t.set('')
+#print(t.get())
+
+Label(text="Click On Button That You want TO active!").pack()
+
+Button(text="convert to csv", command = lambda:Nmea_To_Csv.creat_csv(t.get())).pack()
+Button(text="convert to kml", command=sys.exit).pack()
+Button(text="create a file to db", command=sys.exit).pack()
+Button(text="Exit", command=sys.exit).pack()
+mainloop()
--- Nmea_To_Csv.py
+++ Nmea_To_Csv.py
@@ -1,77 +1,163 @@
 import csv
 from datetime import datetime
 import math
+from tkinter import messagebox
 
 # adapt this to your file
 INPUT_FILENAME = 'in.nmea'
 OUTPUT_FILENAME = 'out.csv'
 
+def creat_csv(FILE_NAME):
+
+
+    if FILE_NAME=='':
+        return
+    if not FILE_NAME.endswith(".nmea"):
+        messagebox.showinfo("info", "message")
+        return
 # open the input file in read mode
-with open("C:/Users/user/Downloads/Desktop/br.txt", 'r') as input_file:
+    with open(FILE_NAME, 'r') as input_file:
 
-    # open the output file in write mode
-    with open("C:/Users/user/Downloads/Desktop/out.csv", 'wt') as output_file:
+        # open the output file in write mode
+        with open("C:/Users/user/Downloads/Desktop/out.csv", 'wt') as output_file:
 
-        # create a csv reader object from the input file (nmea files are basically csv)
-        reader = csv.reader(input_file)
+            # create a csv reader object from the input file (nmea files are basically csv)
+            reader = csv.reader(input_file)
 
-        # create a csv writer object for the output file
-        writer = csv.writer(output_file, delimiter=',', lineterminator='\n')
+            # create a csv writer object for the output file
+            writer = csv.writer(output_file, delimiter=',', lineterminator='\n')
 
-        # write the header line to the csv file
-        writer.writerow(['date_and_time', 'lat', 'lon', 'speed'])
+            # write the header line to the csv file
+            writer.writerow(['date_and_time', 'lat', 'lon', 'speed'])
 
-        # iterate over all the rows in the nmea file
-        for row in reader:
+            # iterate over all the rows in the nmea file
+            for row in reader:
 
-            if not row:
-                continue
-
-            # skip all lines that do not start with $GPRMC
-            if not row[0].startswith('$GPRMC'):
-                continue
-
-            else:
-
-                # for each row, fetch the values from the row's columns
-                # columns that are not used contain technical GPS stuff that you are probably not interested in
-                time = row[1]
-                warning = row[2]
-                lat = row[3]
-                lat_direction = row[4]
-                lon = row[5]
-                lon_direction = row[6]
-                speed = row[7]
-                date =  row[9]
-
-
-                # if the "warning" value is "V" (void), you may want to skip it since this is an indicator for an incomplete data row)
-                if warning == 'V':
+                if not row:
                     continue
 
-                # merge the time and date columns into one Python datetime object (usually more convenient than having both separately)
-                date_and_time = datetime.strptime(date + ' ' + time, '%d%m%y %H%M%S.%f')
+                # skip all lines that do not start with $GPRMC
+                if not row[0].startswith('$GPRMC'):
+                    continue
 
-                # convert the Python datetime into your preferred string format, see http://www.tutorialspoint.com/python/time_strftime.htm for futher possibilities
-                date_and_time = date_and_time.strftime('%y-%m-%d %H:%M:%S.%f')[:-3] # [:-3] cuts off the last three characters (trailing zeros from the fractional seconds)
+                else:
 
-                # lat and lon values in the $GPRMC nmea sentences come in an rather uncommon format. for convenience, convert them into the commonly used decimal degree format which most applications can read.
-                # the "high level" formula for conversion is: DDMM.MMMMM => DD + (YY.ZZZZ / 60), multiplicated with (-1) if direction is either South or West
-                # the following reflects this formula in mathematical terms.
-                # lat and lon have to be converted from string to float in order to do calculations with them.
-                # you probably want the values rounded to 6 digits after the point for better readability.
-                lat = round(math.floor(float(lat) / 100) + (float(lat) % 100) / 60, 6)
-                if lat_direction == 'S':
-                    lat = lat * -1
+                    # for each row, fetch the values from the row's columns
+                    # columns that are not used contain technical GPS stuff that you are probably not interested in
+                    time = row[1]
+                    warning = row[2]
+                    lat = row[3]
+                    lat_direction = row[4]
+                    lon = row[5]
+                    lon_direction = row[6]
+                    speed = row[7]
+                    date =  row[9]
 
-                lon = round(math.floor(float(lon) / 100) + (float(lon) % 100) / 60, 6)
-                if lon_direction == 'W':
-                    lon = lon * -1
 
-                # speed is given in knots, you'll probably rather want it in km/h and rounded to full integer values.
-                # speed has to be converted from string to float first in order to do calculations with it.
-                # conversion to int is to get rid of the tailing ".0".
-                speed = int(round(float(speed) * 1.852, 0))
+                    # if the "warning" value is "V" (void), you may want to skip it since this is an indicator for an incomplete data row)
+                    if warning == 'V':
+                        continue
 
-                # write the calculated/formatted values of the row that we just read into the csv file
-                writer.writerow([date_and_time, lat, lon, speed])
+                    # merge the time and date columns into one Python datetime object (usually more convenient than having both separately)
+                    date_and_time = datetime.strptime(date + ' ' + time, '%d%m%y %H%M%S.%f')
+
+                    # convert the Python datetime into your preferred string format, see http://www.tutorialspoint.com/python/time_strftime.htm for futher possibilities
+                    date_and_time = date_and_time.strftime('%y-%m-%d %H:%M:%S.%f')[:-3] # [:-3] cuts off the last three characters (trailing zeros from the fractional seconds)
+
+                    # lat and lon values in the $GPRMC nmea sentences come in an rather uncommon format. for convenience, convert them into the commonly used decimal degree format which most applications can read.
+                    # the "high level" formula for conversion is: DDMM.MMMMM => DD + (YY.ZZZZ / 60), multiplicated with (-1) if direction is either South or West
+                    # the following reflects this formula in mathematical terms.
+                    # lat and lon have to be converted from string to float in order to do calculations with them.
+                    # you probably want the values rounded to 6 digits after the point for better readability.
+                    lat = round(math.floor(float(lat) / 100) + (float(lat) % 100) / 60, 6)
+                    if lat_direction == 'S':
+                        lat = lat * -1
+
+                    lon = round(math.floor(float(lon) / 100) + (float(lon) % 100) / 60, 6)
+                    if lon_direction == 'W':
+                        lon = lon * -1
+
+                    # speed is given in knots, you'll probably rather want it in km/h and rounded to full integer values.
+                    # speed has to be converted from string to float first in order to do calculations with it.
+                    # conversion to int is to get rid of the tailing ".0".
+                    speed = int(round(float(speed) * 1.852, 0))
+
+                    # write the calculated/formatted values of the row that we just read into the csv file
+                    writer.writerow([date_and_time, lat, lon, speed])
+
+
+
+def main():
+
+# open the input file in read mode
+    with open("C:/Users/user/Downloads/Desktop/br.nmea", 'r') as input_file:
+
+        # open the output file in write mode
+        with open("C:/Users/user/Downloads/Desktop/out.csv", 'wt') as output_file:
+
+            # create a csv reader object from the input file (nmea files are basically csv)
+            reader = csv.reader(input_file)
+
+            # create a csv writer object for the output file
+            writer = csv.writer(output_file, delimiter=',', lineterminator='\n')
+
+            # write the header line to the csv file
+            writer.writerow(['date_and_time', 'lat', 'lon', 'speed'])
+
+            # iterate over all the rows in the nmea file
+            for row in reader:
+
+                if not row:
+                    continue
+
+                # skip all lines that do not start with $GPRMC
+                if not row[0].startswith('$GPRMC'):
+                    continue
+
+                else:
+
+                    # for each row, fetch the values from the row's columns
+                    # columns that are not used contain technical GPS stuff that you are probably not interested in
+                    time = row[1]
+                    warning = row[2]
+                    lat = row[3]
+                    lat_direction = row[4]
+                    lon = row[5]
+                    lon_direction = row[6]
+                    speed = row[7]
+                    date =  row[9]
+
+
+                    # if the "warning" value is "V" (void), you may want to skip it since this is an indicator for an incomplete data row)
+                    if warning == 'V':
+                        continue
+
+                    # merge the time and date columns into one Python datetime object (usually more convenient than having both separately)
+                    date_and_time = datetime.strptime(date + ' ' + time, '%d%m%y %H%M%S.%f')
+
+                    # convert the Python datetime into your preferred string format, see http://www.tutorialspoint.com/python/time_strftime.htm for futher possibilities
+                    date_and_time = date_and_time.strftime('%y-%m-%d %H:%M:%S.%f')[:-3] # [:-3] cuts off the last three characters (trailing zeros from the fractional seconds)
+
+                    # lat and lon values in the $GPRMC nmea sentences come in an rather uncommon format. for convenience, convert them into the commonly used decimal degree format which most applications can read.
+                    # the "high level" formula for conversion is: DDMM.MMMMM => DD + (YY.ZZZZ / 60), multiplicated with (-1) if direction is either South or West
+                    # the following reflects this formula in mathematical terms.
+                    # lat and lon have to be converted from string to float in order to do calculations with them.
+                    # you probably want the values rounded to 6 digits after the point for better readability.
+                    lat = round(math.floor(float(lat) / 100) + (float(lat) % 100) / 60, 6)
+                    if lat_direction == 'S':
+                        lat = lat * -1
+
+                    lon = round(math.floor(float(lon) / 100) + (float(lon) % 100) / 60, 6)
+                    if lon_direction == 'W':
+                        lon = lon * -1
+
+                    # speed is given in knots, you'll probably rather want it in km/h and rounded to full integer values.
+                    # speed has to be converted from string to float first in order to do calculations with it.
+                    # conversion to int is to get rid of the tailing ".0".
+                    speed = int(round(float(speed) * 1.852, 0))
+
+                    # write the calculated/formatted values of the row that we just read into the csv file
+                    writer.writerow([date_and_time, lat, lon, speed])
+
+if __name__ == '__main__':
+    main()
